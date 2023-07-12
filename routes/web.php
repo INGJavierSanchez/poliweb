c0a3f942a05255504031f24314ceecb58df0cbea
INGJavierSanchez <kornjaviersanchezb@gmail.com>
2023-07-11 22:26:05 -0500

    subir ultimo cambios

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,6 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-<<<<<<< HEAD
-
-=======
-/*Route::resource('articulos','App\Http\Controllers\ArticuloController');*/
->>>>>>> f8a659babdb09a1b95fa675def270f9b527b3692
 Route::resource('estudiantes','App\Http\Controllers\EstudianteController');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
